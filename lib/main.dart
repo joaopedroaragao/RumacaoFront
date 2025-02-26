62d8cfaa8a01465e729827fd5df0d1f7773e79d6
João Pedro <jpedroarag@hotmail.com>
2025-02-26 11:15:49 -0300

    Configurando deploy firebase

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,13 @@ import 'package:rumacao_front/constants/app_constants.dart';
 import 'package:rumacao_front/view/home/home_page.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
---
+++ web/firebase_init.js
@@ -0,0 +1,4 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { firebaseConfig } from "./firebase_config.js"
+
+const app = initializeApp(firebaseConfig);
